8ea49060f78a04b4c2da939e4beca4899ad83709
Richa Verma <richa.verma1001@gmail.com>
2020-07-29 22:46:27 -0400

    Modify: File structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import logMessage from './js/logger'
 import './css/style.css'
-import './js/arrays.js';
-import './js/async.js';
-import './js/calc.js';
+import './js/problems/arrays.js';
+import './js/problems/async.js';
+import './js/problems/calc.js';
 //import './js/practice.js';   // Include this file to debug file in browser.
                                // practice.js file will throw a lot of lint errors
                                // Any JS that is not compiled via index.js is currently
